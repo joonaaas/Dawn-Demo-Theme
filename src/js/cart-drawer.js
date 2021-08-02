fdd75b5e773b19bc00e987fa96cae5bf5d525539
Jonas Alolino <jonasqalolino@gmail.com>
2021-08-02 23:51:51 +0800

    add cart drawer

---
+++ src/js/cart-drawer.js
@@ -0,0 +1,25 @@
+const cartIcon = document.querySelector('#cart-icon-bubble');
+const body = document.querySelector('body');
+const preScrim = document.querySelector('.pre-scrim');
+const cartDrawer = document.querySelector('.cart-drawer__container');
+const closeButton = document.querySelector('.cart-drawer__header button');
+
+// Open Drawer
+cartIcon.addEventListener('click', () => {
+	body.classList.toggle('show-cart-drawer');
+	preScrim.classList.toggle('show-cart-drawer');
+	cartDrawer.classList.toggle('show-cart-drawer');
+});
+
+// Close Drawer
+preScrim.addEventListener('click', () => {
+	body.classList.toggle('show-cart-drawer');
+	preScrim.classList.toggle('show-cart-drawer');
+	cartDrawer.classList.toggle('show-cart-drawer');
+});
+
+closeButton.addEventListener('click', () => {
+	body.classList.toggle('show-cart-drawer');
+	preScrim.classList.toggle('show-cart-drawer');
+	cartDrawer.classList.toggle('show-cart-drawer');
+});
---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+let mix = require('laravel-mix');
+
+mix
+	.postCss('src/css/component-cart-drawer.css', 'assets')
+	.js('src/js/cart-drawer.js', 'assets');
